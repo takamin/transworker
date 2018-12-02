95b83f4f3b0a9e0aebeee7f62d7eef0764292798
Koji Takami <vzg03566@gmail.com>
2018-12-02 09:47:03 +0900

    Add tests

--- test/test-class.js
+++ test/test-class.js
@@ -3,4 +3,7 @@ function TestClass() {}
 TestClass.prototype.testMethod = function() {
     return "TestClass.testMethod";
 };
+TestClass.prototype.requestNotify = function(name, message) {
+    /* no implementation on main thread side */
+};
 module.exports = TestClass;
