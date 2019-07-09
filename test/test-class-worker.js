70013914896278b7239312331d7d202821869a1e
Koji Takami <vzg03566@gmail.com>
2019-07-09 19:10:19 +0900

    Fix console.log/warn/error of shared worker before connecting

--- test/test-class-worker.js
+++ test/test-class-worker.js
@@ -3,7 +3,6 @@ require("babel-polyfill");
 const TransWorker = require("../transworker.js");
 const TestClass = require("./test-class.js");
 TestClass.prototype.requestNotify = function(name, message) {
-    console.log(`requestNotify(${name},${message});`);
     this._transworker.postNotify(name, message);
 };
 TransWorker.createWorker(TestClass);
