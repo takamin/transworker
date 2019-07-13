cfae00245f89ce929c6c76c4cada4660fca62819
Koji Takami <vzg03566@gmail.com>
2019-07-13 20:45:29 +0900

    Add test

--- test/transworker-create-interface.js
+++ test/transworker-create-interface.js
@@ -16,82 +16,119 @@ describe("TransWorker", () => {
                 assert.equal(tw._syncType, Function);
             });
         });
-        it("should creates DedicatedWorker specified by options", () => {
-            const tw = TransWorker.createInterface(
-                "test-class-worker-bundle.js", TestClass,
-                { shared: false }
-            );
-            assert.isFalse(tw._shared);
-            assert.equal(tw._syncType, Function);
-        });
-        it("should creates SharedWorker specified by options", () => {
-            const tw = TransWorker.createInterface(
-                "test-class-shared-worker-bundle.js", TestClass,
-                { shared: true }
-            );
-            assert.isTrue(tw._shared);
-            assert.equal(tw._syncType, Function);
-        });
-        it("should creates DedicatedWorker resolved by Callback", () => {
-            const tw = TransWorker.createInterface(
-                "test-class-worker-bundle.js", TestClass,
-                { syncType: TransWorker.SyncTypeCallback }
-            );
-            assert.isFalse(tw._shared);
-            assert.equal(tw._syncType, TransWorker.SyncTypeCallback);
-        });
-        it("should creates DedicatedWorker resolved by Promise", () => {
-            const tw = TransWorker.createInterface(
-                "test-class-worker-bundle.js", TestClass,
-                { syncType: TransWorker.SyncTypePromise }
-            );
-            assert.isFalse(tw._shared);
-            assert.equal(tw._syncType, TransWorker.SyncTypePromise);
-        });
-        it("should creates SharedWorker and Promise wrappers with options", () => {
-            const tw = TransWorker.createInterface(
-                "test-class-shared-worker-bundle.js", TestClass, {
-                    shared: true,
-                    syncType: TransWorker.SyncTypePromise,
-                }
-            );
-            assert.isTrue(tw._shared);
-            assert.equal(tw._syncType, TransWorker.SyncTypePromise);
-        });
-        describe("syncType = Promise", () => {
-            it("should create a wrapper that returns promise", () => {
-                assert.doesNotThrow(() => {
-                    const tw = TransWorker.createInterface(
-                        "test-class-worker-bundle.js", TestClass,
-                        { syncType: TransWorker.SyncTypePromise });
-                    assert.equal(
-                        tw.testMethod().constructor,
-                        TransWorker.SyncTypePromise);
+        describe("Dedicated worker", () => {
+            it("should creates DedicatedWorker specified by options", () => {
+                const tw = TransWorker.createInterface(
+                    "test-class-worker-bundle.js", TestClass,
+                    { shared: false }
+                );
+                assert.isFalse(tw._shared);
+                assert.equal(tw._syncType, Function);
+            });
+            it("should creates DedicatedWorker resolved by Callback", () => {
+                const tw = TransWorker.createInterface(
+                    "test-class-worker-bundle.js", TestClass,
+                    { syncType: TransWorker.SyncTypeCallback }
+                );
+                assert.isFalse(tw._shared);
+                assert.equal(tw._syncType, TransWorker.SyncTypeCallback);
+            });
+            it("should creates DedicatedWorker resolved by Promise", () => {
+                const tw = TransWorker.createInterface(
+                    "test-class-worker-bundle.js", TestClass,
+                    { syncType: TransWorker.SyncTypePromise }
+                );
+                assert.isFalse(tw._shared);
+                assert.equal(tw._syncType, TransWorker.SyncTypePromise);
+            });
+            describe("Invocation", () => {
+                it("should create a wrapper that returns promise", () => {
+                    assert.doesNotThrow(() => {
+                        const tw = TransWorker.createInterface(
+                            "test-class-worker-bundle.js", TestClass,
+                            { syncType: TransWorker.SyncTypePromise });
+                        assert.equal(
+                            tw.testMethod().constructor,
+                            TransWorker.SyncTypePromise);
+                    });
+                });
+                it("should return a value", async () => {
+                    try {
+                        const tw = TransWorker.createInterface(
+                            "/test-class-worker-bundle.js", TestClass,
+                            { syncType: TransWorker.SyncTypePromise });
+                        assert.equal(await tw.testMethod(), "TestClass.testMethod");
+                    } catch (err) {
+                        assert.fail(err.message);
+                    }
+                });
+                it("should transport all parameters to worker even if the callback is omitted (issue#14)", async () => {
+                    try {
+                        const result = await new Promise( resolve => {
+                            const tw = TransWorker.createInterface(
+                                "/test-class-worker-bundle.js", TestClass, null,
+                                { syncType: TransWorker.SyncTypePromise });
+                            tw.subscribe("hello", message => resolve(message));
+                            tw.requestNotify("hello", "transworker");
+                        });
+                        assert.equal(result, "transworker");
+                    } catch (err) {
+                        assert.fail(err.message);
+                    }
                 });
             });
-            it("should callback a return value", async () => {
-                try {
-                    const tw = TransWorker.createInterface(
-                        "/test-class-worker-bundle.js", TestClass,
-                        { syncType: TransWorker.SyncTypePromise });
-                    assert.equal(await tw.testMethod(), "TestClass.testMethod");
-                } catch (err) {
-                    assert.fail(err.message);
-                }
+        });
+        describe("Shared worker", () => {
+            it("should creates SharedWorker specified by options", () => {
+                const tw = TransWorker.createInterface(
+                    "test-class-shared-worker-bundle.js", TestClass,
+                    { shared: true }
+                );
+                assert.isTrue(tw._shared);
+                assert.equal(tw._syncType, Function);
             });
-            it("should transport all parameters to worker even if the callback is omitted (issue#14)", async () => {
-                try {
-                    const result = await new Promise( resolve => {
+            it("should creates SharedWorker and Promise wrappers with options", () => {
+                const tw = TransWorker.createInterface(
+                    "test-class-shared-worker-bundle.js", TestClass, {
+                        shared: true,
+                        syncType: TransWorker.SyncTypePromise,
+                    }
+                );
+                assert.isTrue(tw._shared);
+                assert.equal(tw._syncType, TransWorker.SyncTypePromise);
+            });
+            describe("Invocation", () => {
+                it("should return a value", async () => {
+                    try {
                         const tw = TransWorker.createInterface(
-                            "/test-class-worker-bundle.js", TestClass, null,
-                            { syncType: TransWorker.SyncTypePromise });
-                        tw.subscribe("hello", message => resolve(message));
-                        tw.requestNotify("hello", "transworker");
-                    });
-                    assert.equal(result, "transworker");
-                } catch (err) {
-                    assert.fail(err.message);
-                }
+                            "test-class-shared-worker-bundle.js", TestClass, {
+                                shared: true,
+                                syncType: TransWorker.SyncTypePromise,
+                            }
+                        );
+                        const result = await tw.testMethod();
+                        assert.equal(result, "TestClass.testMethod");
+                    } catch (err) {
+                        assert.fail(err.message);
+                    }
+                });
+                it("should transport all parameters to worker even if the callback is omitted (issue#14)", async () => {
+                    try {
+                        const result = await new Promise(resolve => {
+                            const tw = TransWorker.createInterface(
+                                "test-class-shared-worker-bundle.js", TestClass, {
+                                    shared: true,
+                                    syncType: TransWorker.SyncTypePromise,
+                                }
+                            );
+                            tw.subscribe("hello", message => resolve(message));
+                            tw.requestNotify("hello", "transworker");
+                        });
+                        assert.equal(result, "transworker");
+                    } catch (err) {
+                        assert.fail(err.message);
+                    }
+                });
             });
         });
     });
